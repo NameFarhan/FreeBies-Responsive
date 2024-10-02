25b94acb973349fbbed33a587314f8d525246b62
NameFarhan <Farhanalibwn1215@gmail.com>
2024-10-02 21:26:15 +0500

    --Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+const App = () => {
+  return (
+    <>
+    <Box>
+      <Typography>
+        Hello World
+      </Typography>
+    </Box>
+    </>
+  )
+}
+
+export default App
